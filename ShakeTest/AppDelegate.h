1968e43451e79a9628a94374b91d5f8bb49fa988
iasandcb <iasandcb@gmail.com>
2014-01-16 16:12:13 +0900

    Initial Commit

---
+++ ShakeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShakeTest
+//
+//  Created by T on 2014. 1. 16..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShakeTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShakeTest
+//
+//  Created by T on 2014. 1. 16..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
